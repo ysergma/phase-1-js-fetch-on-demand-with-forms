8e20b635bc247de3e4477e96184fcaa302befe71
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-24 18:25:45 +0100

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('input#searchByID').value;
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then((response)=> response.json())
+        .then((data)=> {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.textContent = data.title
+            summary.textContent = data.summary
+        })
+
+    });
   
 }
 
